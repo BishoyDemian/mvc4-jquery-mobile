dbd19a87259923dbd5f275769e913e7b33047621
Bishoy Demian <bishoy.demian@readify.net>
2012-11-14 17:03:29 +1000

    Inital commit, Empty MVC4 project with updated Nuget packages, and stripped some unneeded references and dependencies

---
+++ MVC4.Bootstrapped/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace MVC4.Bootstrapped
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}

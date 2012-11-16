31e39b09fa02697e1ec5bab441ad3c601570e9a5
Bishoy Demian <bishoy.demian@readify.net>
2012-11-16 12:58:52 +1000

    away from squishit, back to microsoft.web.optimization, and solved a jquery.mobile double(pre)-loading the scripts problem

---
+++ MVC4.Bootstrapped/App_Start/BundleConfig.cs
@@ -0,0 +1,47 @@
+﻿using System.Web;
+using System.Web.Optimization;
+
+namespace MVC4.Bootstrapped
+{
+    public class BundleConfig
+    {
+        // For more information on Bundling, visit http://go.microsoft.com/fwlink/?LinkId=254725
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(
+                new ScriptBundle("~/bundles/scripts/jquery")
+                    .Include("~/Scripts/jquery-{version}.js")
+                );
+
+            bundles.Add(
+                new ScriptBundle("~/bundles/scripts/jquery-mobile")
+                    .Include("~/Scripts/jquery.mobile-{version}.js")
+                );
+
+            bundles.Add(
+                new ScriptBundle("~/bundles/scripts/jquery-val")
+                    .Include("~/Scripts/jquery.unobtrusive*",
+                             "~/Scripts/jquery.validate*")
+                );
+
+            // Use the development version of Modernizr to develop with and learn from. Then, when you're
+            // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
+            bundles.Add(
+                new ScriptBundle("~/bundles/scripts/modernizr")
+                    .Include("~/Scripts/modernizr-*")
+                );
+
+            bundles.Add(
+                new StyleBundle("~/bundles/styles/site"
+                    ).Include("~/Content/Styles/site.css")
+                );
+
+            bundles.Add(
+                new StyleBundle("~/bundles/styles/jquery-mobile")
+                    .Include("~/Content/Themes/jquery.mobile.structure-{version}.css",
+                             "~/Content/Themes/jquery.mobile-{version}.css",
+                             "~/Content/Themes/jquery.mobile.theme-{version}.css")
+                );
+        }
+    }
+}
--- MvcApplication1/MvcApplication1/Global.asax.cs
+++ MvcApplication1/MvcApplication1/Global.asax.cs
@@ -1,9 +1,13 @@
-﻿using System.Web.Http;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
 
-namespace MVC4.Bootstrapped
+namespace MvcApplication1
 {
     // Note: For instructions on enabling IIS6 or IIS7 classic mode, 
     // visit http://go.microsoft.com/?LinkId=9394801
@@ -13,6 +17,7 @@ namespace MVC4.Bootstrapped
         protected void Application_Start()
         {
             AreaRegistration.RegisterAllAreas();
+
             WebApiConfig.Register(GlobalConfiguration.Configuration);
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
